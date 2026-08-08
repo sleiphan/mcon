fa67496ec55ad042b8e6ccd00b60b10669f430c3
Håkon F. Fjellanger <hfredrif@gmail.com>
2026-08-08 14:13:20 +0000

    Add 'gh auth setup-git' to devcontainer config

--- .devcontainer/github-login.sh
+++ .devcontainer/github-login.sh
@@ -1,6 +1,7 @@
 if ! gh auth status >/dev/null 2>&1; then 
   git config --global credential.helper '!gh auth git-credential'
   gh auth login
+  gh auth setup-git
   read -p "Enter your name: " name
   read -p "Enter your email: " email
   git config --global user.name "$name"
