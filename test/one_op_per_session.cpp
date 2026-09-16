5023b0407acb9ba680687085c08def73f9215752
Håkon F. Fjellanger <hfredrif@gmail.com>
2026-09-16 10:29:36 +0000

    Test that a session cannot be closed while it is reading or writing

--- test/one_op_per_session.cpp
+++ test/one_op_per_session.cpp
@@ -31,7 +31,7 @@ void mcon_instance_with_one_connected_client(struct mcon **mcon_dst, int *server
     *client_fd_dst = client_fd;
 }
 
-TEST(mcon, disallow_multiple_live_reads) {
+TEST(session, disallow_multiple_live_reads) {
     struct mcon *mcon;
     int server_socket, epoll_fd, client_fd;
 
@@ -92,7 +92,7 @@ TEST(mcon, disallow_multiple_live_reads) {
     EXPECT_NE(operation_executed, false);
 }
 
-TEST(mcon, disallow_multiple_live_writes) {
+TEST(session, disallow_multiple_live_writes) {
     struct mcon *mcon;
     int server_socket, epoll_fd, client_fd;
 
@@ -152,7 +152,7 @@ TEST(mcon, disallow_multiple_live_writes) {
     EXPECT_NE(operation_executed, false);
 }
 
-TEST(mcon, disallow_multiple_live_closes) {
+TEST(session, disallow_multiple_live_closes) {
     struct mcon *mcon;
     int server_socket, epoll_fd, client_fd;
 
@@ -189,6 +189,124 @@ TEST(mcon, disallow_multiple_live_closes) {
                     EXPECT_LT(mcon_session_close(mcon, evt.session), 0);
                     EXPECT_NE(errno, 0);
                     operation_executed = true;
+                    break;
+                }
+            }
+        }
+
+        EXPECT_GE(mcon_submit(mcon), 0);
+    } while (mcon_active_session_count(mcon) || !operation_executed);
+
+    mcon_destroy(mcon);
+
+    close(epoll_fd), close(server_socket);
+    close(client_fd);
+
+    EXPECT_NE(operation_executed, false);
+}
+
+TEST(session, disallow_close_while_reading) {
+    struct mcon *mcon;
+    int server_socket, epoll_fd, client_fd;
+
+    mcon_instance_with_one_connected_client(&mcon, &server_socket, &epoll_fd, &client_fd);
+    shutdown(client_fd, SHUT_WR);
+
+    const int event_buf_size = 16;
+    struct epoll_event epoll_events[event_buf_size];
+    struct mcon_event mcon_events[event_buf_size];
+
+    const unsigned int transport_buffer_size = 16;
+    char transport_buffer[transport_buffer_size];
+
+    bool operation_executed = false;
+
+    do {
+        int epoll_event_count = epoll_wait(epoll_fd, epoll_events, event_buf_size, 5000);
+        EXPECT_GE(epoll_event_count, 0);
+
+        for (int epoll_event_idx = 0; epoll_event_idx < epoll_event_count; epoll_event_idx++) {
+            int mcon_event_count = mcon_process_event(mcon, epoll_events[epoll_event_idx],
+                                                      mcon_events, event_buf_size);
+            EXPECT_GE(mcon_event_count, 0);
+
+            for (int mcon_event_idx = 0; mcon_event_idx < mcon_event_count; mcon_event_idx++) {
+                struct mcon_event evt = mcon_events[mcon_event_idx];
+
+                switch (evt.type) {
+                case MCON_EVENT_NEW_CONNECTION:
+                    if (operation_executed)
+                        break;
+
+                    EXPECT_GE(mcon_session_read(mcon, evt.session, transport_buffer,
+                                                transport_buffer_size),
+                              0);
+
+                    EXPECT_EQ(mcon_session_close(mcon, evt.session), -1);
+                    EXPECT_NE(errno, 0);
+                    operation_executed = true;
+                    break;
+                case MCON_EVENT_READ_COMPLETE:
+                    mcon_session_close(mcon, evt.session);
+                    break;
+                }
+            }
+        }
+
+        EXPECT_GE(mcon_submit(mcon), 0);
+    } while (mcon_active_session_count(mcon) || !operation_executed);
+
+    mcon_destroy(mcon);
+
+    close(epoll_fd), close(server_socket);
+    close(client_fd);
+
+    EXPECT_NE(operation_executed, false);
+}
+
+TEST(session, disallow_close_while_writing) {
+    struct mcon *mcon;
+    int server_socket, epoll_fd, client_fd;
+
+    mcon_instance_with_one_connected_client(&mcon, &server_socket, &epoll_fd, &client_fd);
+    shutdown(client_fd, SHUT_WR);
+
+    const int event_buf_size = 16;
+    struct epoll_event epoll_events[event_buf_size];
+    struct mcon_event mcon_events[event_buf_size];
+
+    const unsigned int transport_buffer_size = 16;
+    char transport_buffer[transport_buffer_size];
+
+    bool operation_executed = false;
+
+    do {
+        int epoll_event_count = epoll_wait(epoll_fd, epoll_events, event_buf_size, 5000);
+        EXPECT_GE(epoll_event_count, 0);
+
+        for (int epoll_event_idx = 0; epoll_event_idx < epoll_event_count; epoll_event_idx++) {
+            int mcon_event_count = mcon_process_event(mcon, epoll_events[epoll_event_idx],
+                                                      mcon_events, event_buf_size);
+            EXPECT_GE(mcon_event_count, 0);
+
+            for (int mcon_event_idx = 0; mcon_event_idx < mcon_event_count; mcon_event_idx++) {
+                struct mcon_event evt = mcon_events[mcon_event_idx];
+
+                switch (evt.type) {
+                case MCON_EVENT_NEW_CONNECTION:
+                    if (operation_executed)
+                        break;
+
+                    EXPECT_GE(mcon_session_write(mcon, evt.session, transport_buffer,
+                                                 transport_buffer_size),
+                              0);
+
+                    EXPECT_EQ(mcon_session_close(mcon, evt.session), -1);
+                    EXPECT_NE(errno, 0);
+                    operation_executed = true;
+                    break;
+                case MCON_EVENT_WRITE_COMPLETE:
+                    mcon_session_close(mcon, evt.session);
                     break;
                 }
             }
